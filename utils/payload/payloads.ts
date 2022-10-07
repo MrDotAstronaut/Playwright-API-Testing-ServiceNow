60da171836d5c9fcca33466563a885dd17f3b3b6
MrDotAstronaut <mrdotastronaut@gmail.com>
2022-10-07 13:58:30 +0530

    Final

--- utils/payload/payloads.ts
+++ utils/payload/payloads.ts
@@ -1,14 +1,14 @@
 export const payload = {
     createPayload : {
-        "short_description": "Mouse issue",
-        "category": "hardware"
+        short_description: "Mouse issue",
+        category: "hardware"
     },
     modifyPayload : {
-        "short_description": "Microsoft office not working",
-        "category": "software"
+        short_description: "Microsoft office not working",
+        category: "software"
     },
     updatePayload : {
-        "short_description": "Wifi not working",
-        "category": "network"
+        short_description: "Wifi not working",
+        category: "network"
     },
 }
